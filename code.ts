7c4d6d72766b5cd0c5925168715011dab83e5234
Unoriginal02 <jairo.contact@gmail.com>
2023-06-27 06:28:56 +0200

    Better Variable Support

--- code.ts
+++ code.ts
@@ -61,13 +61,14 @@ figma.ui.onmessage = (msg) => {
 
 
   // This function traverses a FrameNode and identifies nodes with applied color styles and nodes without
-  function findColorStyles(group: FrameNode): { colorStyles: { name: string; nodes: any[]; isRemote: boolean; styleType: string }[]; noColorStyles: { name: string; nodes: any[]; styleType: string }[]; boundVariables: { name: string; hexColor: string; nodes: any[]; styleType: string }[]; } {
-
+  function findColorStyles(group: FrameNode, msg: string): { colorStyles: { name: string; nodes: any[]; isRemote: boolean; styleType: string }[]; noColorStyles: { name: string; nodes: any[]; styleType: string }[]; boundVariables: { name: string; hexColor: string; nodes: any[]; styleType: string }[]; } {
+    console.log("findColorStyles recieved:", msg);
 
     // Initialize arrays to hold nodes with and without color styles
     const colorStyles: { name: string; hexColor: string; nodes: any[]; isRemote: boolean; styleType: string }[] = [];
     const noColorStyles: { name: string; hexColor: string; nodes: any[]; styleType: string }[] = [];
     const boundVariables: { name: string; hexColor: string; nodes: any[]; isRemote: boolean; styleType: string }[] = [];
+    const visitedNodes = new Set<string>();
 
     // Type guard to check if styleId is a string
     function isStyleIdString(styleId: string | typeof figma.mixed): styleId is string {
@@ -115,6 +116,56 @@ figma.ui.onmessage = (msg) => {
       }
     }
 
+    function searchAllColorStyles(node: SceneNode): void {
+      console.log("GetAllStyles function message triggered");
+      // Skip processing hidden nodes
+      if (!node.visible) {
+        return;
+      }
+      // console.log('Processing node:', node.id, node.type);
+      if (visitedNodes.has(node.id)) {
+        // console.log('Skipping node:', node.id);
+        return;
+      }
+      visitedNodes.add(node.id);
+
+      if ("fills" in node && Array.isArray(node.fills) && node.fills.length > 0) {
+        const fill = node.fills[0];
+        if (fill.type === "SOLID") {
+          if (node.boundVariables?.['fills']) {
+            // Process bound variables for fills
+            node.boundVariables?.['fills'].forEach(variableAlias => {
+              const fillVariableId = variableAlias.id;
+              processBoundVariables(node, fillVariableId, "fill");
+            });
+          } else {
+            processColorStyles(node, fill.color, isStyleIdString(node.fillStyleId) ? node.fillStyleId : null, "fill");
+          }
+        }
+      }
+
+      if ("strokes" in node && Array.isArray(node.strokes) && node.strokes.length > 0) {
+        const stroke = node.strokes[0];
+        if (stroke.type === "SOLID") {
+          if (node.boundVariables?.['strokes']) {
+            // Process bound variables for strokes
+            node.boundVariables?.['strokes'].forEach(variableAlias => {
+              const fillVariableId = variableAlias.id;
+              processBoundVariables(node, fillVariableId, "stroke");
+            });
+          } else {
+            processColorStyles(node, stroke.color, isStyleIdString(node.strokeStyleId) ? node.strokeStyleId : null, "stroke");
+          }
+        }
+      }
+
+      if ("children" in node) {
+        for (const child of node.children) {
+          searchAllColorStyles(child);
+        }
+      }
+    }
+
     // Function to process color styles - adds nodes to either colorStyles or noColorStyles array
     function processColorStyles(node: BaseNode, color: RGBA, styleId: string | null, styleType: string) {
       const hexColor = rgbaToHex(color);
@@ -141,7 +192,6 @@ figma.ui.onmessage = (msg) => {
 
     function processBoundVariables(node: BaseNode, variableId: string, styleType: string) {
       const variable = figma.variables.getVariableById(variableId);
-      // console.log("les variables:",variable);
 
       if (isSceneNode(node)) {
         if (variable !== null) {
@@ -150,7 +200,6 @@ figma.ui.onmessage = (msg) => {
           let rgbaColor: RGBA;
 
           if (typeof variableValue === 'string') {
-            // Convert the hex string to an RGBA object
             rgbaColor = hexToRgba(variableValue);
           } else if (isRgba(variableValue)) {
             rgbaColor = variableValue;
@@ -160,11 +209,22 @@ figma.ui.onmessage = (msg) => {
 
           const hexColor = rgbaToHex(rgbaColor);
 
-          const existingVariable = boundVariables.find((entry) => entry.name === variable.name && entry.styleType === styleType);
+          const existingVariable = boundVariables.find(
+            (entry) =>
+              entry.name === variable.name &&
+              entry.hexColor === hexColor &&
+              entry.styleType === styleType
+          );
           if (existingVariable) {
             existingVariable.nodes.push(getNodeToAppend(node));
           } else {
-            boundVariables.push({ name: variable.name, hexColor, nodes: [getNodeToAppend(node)], isRemote: variable.remote, styleType });
+            boundVariables.push({
+              name: variable.name,
+              hexColor,
+              nodes: [getNodeToAppend(node)],
+              isRemote: variable.remote,
+              styleType,
+            });
           }
 
         } else {
@@ -173,12 +233,16 @@ figma.ui.onmessage = (msg) => {
       } else {
         console.log('Node is not a SceneNode');
       }
-
-
     }
 
+
+
     // Start the color style search in the group
-    searchColorStyles(group);
+    if (msg === "getStyles" || msg === "refresh") {
+      searchColorStyles(group);
+    } else if (msg === "getAllStyles") {
+      searchAllColorStyles(group as SceneNode);
+    }
     // console.log("boundVariables:",boundVariables);
     // console.log("colorStyles:",colorStyles);
     // console.log("noColorStyles:",noColorStyles);
@@ -192,13 +256,15 @@ figma.ui.onmessage = (msg) => {
       });
     });
 
+    console.log("Find colors results:", colorStyles, filteredNoColorStyles, boundVariables);
+
     return { colorStyles, noColorStyles: filteredNoColorStyles, boundVariables };
 
   }
 
   // This function traverses a FrameNode and identifies nodes with applied font styles and nodes without
-  function findFontStyles(group: FrameNode): { fontStyles: { name: string; nodes: any[] }[]; noFontStyles: { name: string; nodes: any[] }[] } {
-
+  function findFontStyles(group: FrameNode, msg: string): { fontStyles: { name: string; nodes: any[] }[]; noFontStyles: { name: string; nodes: any[] }[] } {
+    console.log("findFontStyles messages received:", msg);
     // Initialize arrays to hold nodes with and without font styles
     const fontStyles: { name: string; nodes: any[]; isRemote: boolean }[] = [];
     const noFontStyles: { name: string; nodes: any[] }[] = [];
@@ -252,15 +318,66 @@ figma.ui.onmessage = (msg) => {
       }
     }
 
+    // Recursive function to search for font styles in the children of a node
+    function searchAllFontStyles(node: BaseNode): void {
+      console.log("searchAllFontStyles function triggered");
+
+      if (
+        node.type === "TEXT" &&
+        node.fontName &&
+        typeof node.fontName === "object"
+      ) {
+        let fontStyleName = "";
+
+        if (node.textStyleId && typeof node.textStyleId === "string") {
+          const textStyle = figma.getStyleById(node.textStyleId);
+          if (textStyle) {
+            fontStyleName = textStyle.name;
+            const existingStyle = fontStyles.find((style) => style.name === fontStyleName);
+            if (existingStyle) {
+              existingStyle.nodes.push(getNodeToAppend(node));
+            } else {
+              fontStyles.push({ name: fontStyleName, nodes: [getNodeToAppend(node)], isRemote: textStyle.remote });
+            }
+          }
+        } else {
+          fontStyleName = `${node.fontName.family} ${node.fontName.style}`;
+          if (node.fontSize) {
+            fontStyleName += ` - ${String(node.fontSize)}px`;
+          }
+
+          const existingStyle = noFontStyles.find((style) => style.name === fontStyleName);
+          if (existingStyle) {
+            existingStyle.nodes.push(getNodeToAppend(node));
+          } else {
+            noFontStyles.push({ name: fontStyleName, nodes: [getNodeToAppend(node)] });
+          }
+        }
+      }
+
+      if ("children" in node) {
+        for (const child of node.children) {
+          searchAllFontStyles(child);
+        }
+      }
+
+    }
+
     // Start the font style search in the group
-    searchFontStyles(group);
+    if (msg === "getStyles" || msg === "refresh") {
+      searchFontStyles(group);
+    } else if (msg === "getAllStyles") {
+      searchAllFontStyles(group);
+    }
+
+    console.log("fontStyles result:", fontStyles, noFontStyles);
 
     // Return the results - font styles and no font styles
     return { fontStyles, noFontStyles };
   }
 
   // Code to execute when a message of type 'getStyles' is received
-  if (msg.type === 'getStyles') {
+  if (msg.type === 'getStyles' || msg.type === 'getAllStyles') {
     const selection = figma.currentPage.selection;
 
     // 1- Take the current selection (one or multiple objects)
@@ -313,8 +430,8 @@ figma.ui.onmessage = (msg) => {
 
       // 3- Execute the function as is
       // Find the color and font styles used in the group
-      const colorStylesResults = findColorStyles(group);
-      const fontStylesResults = findFontStyles(group);
+      const colorStylesResults = findColorStyles(group, msg.type);
+      const fontStylesResults = findFontStyles(group, msg.type);
 
       // Extract the results from the return objects
       const variables = colorStylesResults.boundVariables;
@@ -345,14 +462,13 @@ figma.ui.onmessage = (msg) => {
     }
   }
 
-  // Refresh
   if (msg.type === 'refresh') {
     const groupName = "Matcher Workbench";
     const group = figma.currentPage.findOne(node => node.type === 'FRAME' && node.name === groupName) as FrameNode;
 
     if (group) {
-      const colorStylesResults = findColorStyles(group);
-      const fontStylesResults = findFontStyles(group);
+      const colorStylesResults = findColorStyles(group, msg.type);
+      const fontStylesResults = findFontStyles(group, msg.type);
 
       const variables = colorStylesResults.boundVariables;
       const colorStyles = colorStylesResults.colorStyles;
